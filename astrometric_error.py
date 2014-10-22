4822688eca018f4eb3dd3cbb1485692e80c5a2b1
Jun Cheng <cheng109@purdue.edu>
2014-10-22 18:03:08 -0400

    Update for astrometricError project

---
+++ astrometric_error.py
@@ -0,0 +1,103 @@
+"""
+  @package 
+  @file moffatFitting.py
+  @brief compute the fwhm from the 'segmentation.fits' and 'image.fits'
+ 
+  @brief Created by:
+  @author Jun Cheng (Purdue)
+  
+  @warning This code is not fully validated
+  and not ready for full release.  Please
+  treat results with caution.
+
+Usage: python sex_validation.py <image.fits> <test.cat> <result.txt>
+ 
+Notes:
+1. This code is mainly used to validate that central value from sextractor is CORRECT.
+2. It takes <image.fits> and <test.cat> from sextractor as input file, and output the results.
+3. This code can save all the moffat fitting results of stars throughout the whole image.
+
+"""
+import star_list
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+def angle_distance(A, B):
+    A[:] = [x/180*np.pi for x in A]
+    B[:] = [x/180*np.pi for x in B]
+    return np.arccos(np.cos(A[1])*np.cos(B[1])*np.cos(A[0]-B[0]) + np.sin(A[1])*np.sin(B[1]))*180/np.pi*3600000    # in arcsec
+
+def update_grid(starList, spacing):
+    temp_CenterX = 10000
+    temp_CenterY = 10000
+    temp_RA = 0
+    temp_DEC = 0
+
+    for i in range(len(starList)):
+        if (starList[i].sex_CenterX+starList[i].sex_CenterY)<(temp_CenterX+temp_CenterY):
+             temp_CenterX = starList[i].sex_CenterX
+             temp_CenterY = starList[i].sex_CenterY
+             temp_RA = starList[i].sex_RA
+             temp_DEC = starList[i].sex_DEC
+
+    for i in range(len(starList)):
+        starList[i].RA_grid=np.round((starList[i].sex_RA-temp_RA)/spacing)
+        starList[i].DEC_grid=np.round((starList[i].sex_DEC-temp_DEC)/spacing)
+        #plt.plot(starList[i].RA_grid, starList[i].DEC_grid, '*b')
+    #plt.show()
+
+
+def createDistanceList(starList):
+    global unit_dist
+    distance = [];
+    unit = [];
+    for i in range(len(starList)-1):
+        for j in range(i+1, len(starList)):
+            A = starList[i]
+            B = starList[j]
+            if int(A.RA_grid)==int(B.RA_grid):
+                distance.append(angle_distance([A.sex_RA, A.sex_DEC], [B.sex_RA, B.sex_DEC]))
+                unit.append(int(abs(B.DEC_grid - A.DEC_grid)))
+
+    x=list(set(unit))
+    std = [0]*len(x)
+    d = [[] for i in range (len(x))]
+    for i in range(len(unit)):
+        d[unit[i]-1].append(distance[i])
+
+    for j in range(len(d)):
+        std[j] = np.std(d[j])
+    return x, std
+
+def plotAstroError(x,std,labelName):
+
+    plt.plot(x,std, '*--', label='lines')
+    plt.xlabel("Seperation (arcsec)")
+    plt.ylabel("STD (MAS)")
+    #patch = mpatches.Patch(color='red', label=labelName)
+    #plt.legend(handles=[patch])
+    #plt.show()
+
+def writeToFile(x, std, fileName):
+    f = open(fileName[:-5]+"_output.txt",'w')
+    for i in range(len(x)):
+        f.write(str(x[i]) + "\t" + str(std[i])+"\n")
+    f.close()
+
+if __name__=="__main__":
+    spacing = 0.0077
+    winX=10
+    winY=10
+    xlim = [20,4000-20]
+    ylim = [20,4000-20]
+    starListDict = star_list.generateStarListDictionary(sys.argv[1:], (winX, winY), (xlim, ylim), sex=True, Moffat=False)
+    for key in starListDict:
+        update_grid(starListDict[key], spacing)
+        x, std = createDistanceList(starListDict[key])
+        x=[spacing*i*3600 for i in x]
+        writeToFile(x, std, key)
+
+        plotAstroError(x,std, key)
+    plt.show()
---
+++ star_list.py
@@ -0,0 +1,181 @@
+"""
+  @package 
+  @file star_list.py
+  @brief compute the fwhm from the 'segmentation.fits' and 'image.fits'
+ 
+  @brief Created by:
+  @author Jun Cheng (Purdue)
+  
+  @warning This code is not fully validated
+  and not ready for full release.  Please
+  treat results with caution.
+
+Usage: python sex_validation.py <image.fits> <test.cat> <result.txt>
+ 
+Notes:
+
+1. This is a procedure to create a dictionary {'fitsFileName01': StarList01, 'fitsFileName02': StarList02,......}
+2. If you don't have a .cat file from SExtractor, then you can create one by setting 'sex=True'
+3. If you want to used Moffat fitted value instead of the one from SExtrator, specify 'Moffat=True'
+
+"""
+import sys, subprocess
+from astropy.io import fits
+from astropy.wcs import WCS
+from aspylib import astro
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Star:
+    def __init__(self, ID, sex_CenterX, sex_CenterY, sex_RA, sex_DEC, data, wcs, origin, windows):
+        self.winX =windows[0]
+        self.winY =windows[1]
+
+        self.ID = ID
+        self.sex_CenterX = sex_CenterX
+        self.sex_CenterY = sex_CenterY
+        self.sex_RA = sex_RA
+        self.sex_DEC = sex_DEC
+        self.data = data
+        self.wcs = wcs
+        self.origin = origin
+
+        self.RA_grid = 0
+        self.DEC_grid = 0
+
+        self.moffat_A = 0
+        self.moffat_B = 0
+        self.moffat_C = 0
+        self.moffat_max = 0
+        self.moffat_background = 0
+        self.moffat_amplitude = 0
+        self.moffat_CenterX = 0
+        self.moffat_CenterY = 0
+        self.moffat_smallest_fwhm =0
+        self.moffat_largest_fwhm=0
+        self.moffat_phi=0
+        self.moffat_ellipticity =0
+        self.moffat_red_chi_square = 0
+        self.moffat_beta = 0
+
+
+    def print_star(self):
+        print("ID=", self.ID)
+
+    def update_wcs(self):
+        self.moffat_RA, self.moffat_DEC = self.wcs.all_pix2world(self.moffat_global_CenterX,self.moffat_global_CenterY,0)
+
+    def update_moffat(self):
+        moffat_results = astro.fit_moffat_elliptical([0,0],self.data)
+
+        self.moffat_max = moffat_results[0]
+        self.moffat_background = moffat_results[1]
+        self.moffat_amplitude = moffat_results[2]
+        self.moffat_CenterX = moffat_results[3]
+        self.moffat_CenterY = moffat_results[4]
+        self.moffat_smallest_fwhm = moffat_results[5]
+        self.moffat_largest_fwhm  = moffat_results[6]
+        self.moffat_phi  = moffat_results[7]
+        self.moffat_beta = moffat_results[8]
+
+        phi = moffat_results[7]
+        beta = moffat_results[8]
+
+        denom = 2*np.sqrt(np.power(2,1/self.moffat_beta)-1)
+        alpha1=self.moffat_smallest_fwhm/denom
+        alpha2=self.moffat_largest_fwhm/denom
+
+        self.moffat_A = (np.cos(phi)/alpha1)**2 + (np.sin(phi)/alpha2)**2
+        self.moffat_B = (np.sin(phi)/alpha1)**2 + (np.cos(phi)/alpha2)**2
+        self.moffat_C = 2*np.sin(phi)*np.cos(phi)*(1/alpha1**2-1/alpha2**2)
+        self.moffat_global_CenterX = self.origin[1] + self.moffat_CenterY
+        self.moffat_global_CenterY = self.origin[0] + self.moffat_CenterX
+
+
+    def plotFitting(self):
+
+        data = self.data
+        xmargin = np.sum(data,axis=1)
+        ymargin = np.sum(data,axis=0)
+
+        xx = np.arange(0,len(xmargin))-self.moffat_CenterX-0.5
+        yy = np.arange(0,len(ymargin))-self.moffat_CenterY-0.5
+
+        f, (ax3,ax4)=plt.subplots(1,2,sharex='col', sharey='row')
+        xx3=np.arange(-1*self.winX, self.winX,.1)
+        yy3=np.arange(-1*self.winY, self.winY,.1)
+        X, Y = np.meshgrid(xx3, yy3)
+        moffat_Z = self.moffat_background + self.moffat_amplitude/np.power(1+self.moffat_A*X**2
+                                                                    +self.moffat_B*Y**2
+                                                                    +self.moffat_C*X*Y,
+                                                                    self.moffat_beta)
+        moffat_xmargin = np.sum(moffat_Z, axis=1)/10
+        moffat_ymargin = np.sum(moffat_Z, axis=0)/10
+
+        ax3.set_xlabel("X")
+        ax4.set_xlabel("Y")
+        ax3.bar(xx,xmargin)
+        ax3.plot(xx3,moffat_xmargin,'-r', linewidth=3.0)
+        ax3.set_xlim([-10,10])
+        ax4.bar(xx,ymargin)
+        ax4.plot(xx3,moffat_ymargin,'-r', linewidth=3.0)
+        ax4.set_xlim([-10,10])
+        imgName = "fitting_chip_"+"_ID_"+str(self.ID) +".eps"
+        plt.show()
+        #plt.savefig(imgName, format='eps', dpi=1000)
+        #plt.close()
+
+
+def generateStarList(fitsFileName, catFileName, (winX, winY), (limX, limY), Moffat):
+    starList=[]
+    catFile=open(catFileName,'r')
+    imgFile = fits.open(fitsFileName)
+    wcs = WCS(fitsFileName)
+    image_map = imgFile[0].data
+
+
+    for line in catFile.readlines():
+        if line[0]!='#':
+            temp=line.split()
+            ID = float(temp[0])
+            sex_CenterX = float(temp[1])
+            sex_CenterY = float(temp[2])
+            sex_RA  = float(temp[3])
+            sex_DEC = float(temp[4])
+            if sex_CenterY>limY[0] and sex_CenterY< limY[1] and sex_CenterX>limX[0] and sex_CenterX < limX[1]:
+
+
+
+                data = image_map[np.floor(sex_CenterY-winY):np.floor(sex_CenterY+winY)+1,np.floor(sex_CenterX-winX):np.floor(sex_CenterX+winX)+1]
+                origin = (np.floor(sex_CenterY)-winY, np.floor(sex_CenterX)-winX)
+
+
+                star = Star(ID, sex_CenterX, sex_CenterY, sex_RA, sex_DEC, data, wcs, origin, (winX, winY))
+                if Moffat==True:
+                    star.update_moffat()
+
+                starList.append(star)
+
+    catFile.close()
+    return starList
+
+
+def generateStarListDictionary(fitsNameList, (winX, winY), (xlim, ylim), sex, Moffat ):
+    keys = fitsNameList
+    starDict = dict.fromkeys(keys)
+    for i in range(len(fitsNameList)):
+        fitsName = fitsNameList[i]
+        catName = fitsName[:-5] +"_sex.cat"
+        if sex ==True:
+            subprocess.call("sex " + fitsName + " -CATALOG_NAME " + catName, shell=True)
+        starList= generateStarList(fitsName, catName, (winX, winY), (xlim, ylim), Moffat)
+        starDict[fitsName] = starList
+    return starDict
+
+if __name__=="__main__":
+    winX=10
+    winY=10
+    #windows = (winX, winY)
+    xlim = [60,40000-60]
+    ylim = [30,600-60]
+    dict = generateStarListDictionary(sys.argv[1:],(winX, winY), (xlim, ylim), sex=True, Moffat=False)
